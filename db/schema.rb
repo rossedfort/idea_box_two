f1965b59e015422529fb5069ce96661f98a27cdb
rossedfort <rossedfort@gmail.com>
2016-02-01 11:57:59 -0700

    add default quality for ideas

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 20160201184745) do
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.integer  "quality"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "quality",    default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
