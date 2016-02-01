2a0b414a9a7a623d34950dbfd291d16c7ddbb5fd
rossedfort <rossedfort@gmail.com>
2016-02-01 12:15:54 -0700

    add root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "root#root"
-  
+  root "root#index"
+
   namespace :api do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :update, :destroy], defaults: {format: :json}
