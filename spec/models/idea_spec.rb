8645f5b8cf3fe0c83c278038894edc9d97a50eda
rossedfort <rossedfort@gmail.com>
2016-02-03 21:33:36 -0700

    remove unnecessary helpers

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
+  it 'creates successful idea' do
+    idea = Idea.new(title: "idea", body: "so good")
+    expect(idea).to be_valid
+  end
+
+  it 'cannot create idea without title' do
+    idea = Idea.new(body: "so good")
+    expect(idea).to_not be_valid
+  end
+
+  it 'cannot create idea without body' do
+    idea = Idea.new(title: 'idea')
+    expect(idea).to_not be_valid
+  end
 end
