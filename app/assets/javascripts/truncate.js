cba8d0ae7111634d7faad217343240140efec7ff
rossedfort <rossedfort@gmail.com>
2016-02-03 18:52:39 -0700

    truncate functional

--- app/assets/javascripts/truncate.js
+++ app/assets/javascripts/truncate.js
@@ -2,7 +2,7 @@ function truncate(){
   $('.idea-body').each(function(){
     var text = $(this).text()
     if (text.length > 100) {
-      $(this).text(text.replace(text, text.slice(0, 97) + "..."));
+      $(this).text(text.replace(text, $.trim($(this).text()).substring(0, 100).split(" ").slice(0, -1).join(" ") + "..." ));
     }
   });
 }
